851076d958b1820cddc9161ae10a119c20662873
Nicolai Eckhardt <Nicolai.b.eckhardt@gmail.com>
2025-03-16 15:43:40 +0100

    1543_1603
    Initial unstable LQR simulink implementation

--- Mandatory Assignment 2025 - Part A/InputValues.m
+++ Mandatory Assignment 2025 - Part A/InputValues.m
@@ -1,4 +1,4 @@
-function  InputValues()
+function [J_1, J_2, J_3, k_1, k_2, b_1, b_2, b_3, T_Cp, T_Cm] = InputValues()
 %UNTITLED Summary of this function goes here
 load('ECP_values.mat');
 load('ECP502Data.mat');
@@ -16,53 +16,6 @@ T_Cm = ECP_values(13);          % Disk 1 Coulomb friction in negative direction
 atan_scale = 100;               % Sign approximation factor
 w_th = 0.75;                    % Threshold angular velocity rad/s
 
-
-A = [0 1 0 0 0 0
-    -k_1/J_1 -b_1/J_1 k_1/J_1 0 0 0
-    0 0 0 1 0 0
-    k_1/J_2 0 -(k_1+k_2)/J_2 -b_2/J_2 k_2/J_2 0
-    0 0 0 0 0 1
-    0 0 k_2/J_3 0 -k_2/J_3 -b_3/J_3];
-B = [0 0
-    1/J_1 0
-    0 0
-    0 1/J_2
-    0 0
-    0 0];
-C = [  1 0 0 0 0 0
-       0 0 1 0 0 0
-       0 0 0 0 1 0 ];
-D = [0 0
-     0 0
-     0 0];
-
-% Disturbance matrices               %why is it not just 1??? 
-E_x = [ 0 
-        1/J_1 
-        0 
-        0 
-        0 
-        0 ];
-E_y = [ 0 
-        0 
-        0 ];
-
-% Actuator and system fault matrix (6x5) %f ={row = size of input output, cols = size of state] 
-F_x = [0 0 0 0 0  
-       0 0 0 1/J_1 0   
-       0 0 0 0 0   
-       0 1/J_2 0 0 1/J_2   
-       0 0 0 0 0  
-       0 0 0 0 0 ];
-
-
-% Sensor fault matrices Skal være 3x5
-F_y = [ 1 0 0 0 0
-        0 1 0 0 0
-        0 0 1 0 0 ];
-
-H_ry = [k_1/J_2 (-k_1-k_2-b_2*s)/J_2-s^2 k_2/J_2; ...
-   0 k_2/J_3 (-k_2-b_3*s)/J_3-s^2]
 
 end
 
